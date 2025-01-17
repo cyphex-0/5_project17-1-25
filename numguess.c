8bc9feaefd28260896f0b3b9b12eaa069df70cdb
cyphex <contact.makhdum@gmail.com>
2025-01-17 22:03:42 +0600

    add time(NULL)

--- numguess.c
+++ numguess.c
@@ -2,22 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
     int number, guess, attempts = 0;
-    srand(time(0));
+    srand(time(NULL));
     number = rand() % 100 + 1;
 
     printf("Guess the number (between 1 and 100):\n");
-    do {
+    do
+    {
         attempts++;
         printf("Enter your guess: ");
         scanf("%d", &guess);
 
-        if (guess < number) {
+        if (guess < number)
+        {
             printf("Too low!\n");
-        } else if (guess > number) {
+        }
+        else if (guess > number)
+        {
             printf("Too high!\n");
-        } else {
+        }
+        else
+        {
             printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
         }
     } while (guess != number);
